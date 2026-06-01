8d5ebbcddbb6eba7e11aaceb59e1271342c7a0dc
Ismail Marmoush <marmoushismail@gmail.com>
2026-06-01 19:59:30 +0300

    analytics and removing tailwind

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,9 @@
 <template>
-  <div class="h-screen w-screen flex items-center justify-center bg-[#fef7ed]">
-    <img src="/src/assets/images/logo.jpg" alt="elmarket.app Logo" >
+  <div class="home-container">
+    <img src="/src/assets/images/logo.jpg" alt="elmarket.app Logo">
   </div>
 </template>
 
 <script setup lang="ts">
 document.title = 'El Market';
-
-import {ref} from 'vue'
-
 </script>
